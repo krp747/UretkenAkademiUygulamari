25218e009e35cbc83bd641d3d40f386615087148
Mehmet Enes Turhan <114769038+krp747@users.noreply.github.com>
2023-11-14 18:10:02 +0300

    Update app.js
    kod açıklaması ekledim

--- odev/app.js
+++ odev/app.js
@@ -6,8 +6,8 @@ tuslar.forEach(function (tus) {
     tus.addEventListener('click', () => {
         const islem = tus.value;
         if (islem === 'clear') {
-            giris = '';
-        } else if (islem === '=') {
+            giris = '';//bütün ifadeyi ve devamında ekranıda temizlemekte.
+        } else if (islem === '=') {//eşittire basıldığında metodu string türündeki matematiksel işlemi yapması için çağırıyor.
             try {
                 giris = eval(giris);
             } catch (error) {
@@ -20,3 +20,6 @@ tuslar.forEach(function (tus) {
         ekran.value = giris;
     });
 });
+/*Basılan her tuşun değeri öncelikle bir değişkene(islem) alınmakta ve bu değişken her tuş basımından sonra giris değişkenine önceki değerler ile birleştirilmekte ve giriş değişkeni*/
+/*ekrana yazdırılmakta*/
+//temizle tuşuna basıldığında ise giris değişkeni sıfırlanmakta
